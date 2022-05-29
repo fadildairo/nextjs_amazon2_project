57112acd87d0fceaf7fddafc962419a589eaab27
Fadil Dairo <fadildairo@gmail.com>
2022-05-29 12:39:09 +0100

    Updated frontend and nextauth authentication

---
+++ nextjs-amazon-portfolio-project/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6UzFrOr8acd-m1qkovy-XlBuwAZSlycY",
+  authDomain: "nextjs--project.firebaseapp.com",
+  projectId: "nextjs-amazon-project",
+  storageBucket: "nextjs-amazon-project.appspot.com",
+  messagingSenderId: "690674103868",
+  appId: "1:690674103868:web:98af4ffdd399011cb6f539"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
