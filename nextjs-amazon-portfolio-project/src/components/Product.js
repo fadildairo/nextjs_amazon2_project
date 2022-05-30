f5946ce24089b6ce048add879b704d1fde5f9258
Fadil Dairo <fadildairo@gmail.com>
2022-05-30 07:11:52 +0100

    Checkout & Google Authentication

--- nextjs-amazon-portfolio-project/src/components/Product.js
+++ nextjs-amazon-portfolio-project/src/components/Product.js
@@ -2,11 +2,15 @@ import Image from "next/image";
 import { StarIcon } from "@heroicons/react/solid";
 import { useState } from "react";
 import Currency from 'react-currency-formatter';
+import { useDispatch } from "react-redux";
+import { addToBasket } from "../slices/basketSlice";
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
 
 function Product({id, title, price, description, category, image}) {
+    const dispatch = useDispatch();
+
     const [rating] = useState(
         Math.floor(Math.random()*(MAX_RATING - MIN_RATING + 1)) + MIN_RATING
     );
@@ -14,6 +18,22 @@ function Product({id, title, price, description, category, image}) {
     const [hasPrime] = useState(
         Math.random() < 0.5
     );
+
+    const addItemToBasket = () => {
+        const product = {
+            id,
+            title, 
+            price, 
+            rating,
+            description, 
+            category, 
+            image,
+            hasPrime,
+        };
+
+        //Sending the product as an action to the REDUX store. i.e the basket slice
+        dispatch(addToBasket(product));
+    };
 
   return (
     <div className="relative flex flex-col m-5 bg-white z-30 p-10 ">
@@ -37,7 +57,7 @@ function Product({id, title, price, description, category, image}) {
             <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
         </div>
         )}
-        <button className="mt-auto button">Add to Basket</button>
+        <button onClick={addItemToBasket} className="mt-auto button">Add to Basket</button>
     </div>
   )
 }
