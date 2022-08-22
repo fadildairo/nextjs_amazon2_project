e8fc7dc34eadbbfc16f710805d4070e8b6345293
Fadil Dairo <fadildairo@gmail.com>
2022-08-22 09:46:25 +0100

    Update

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import Auth0 from "next-auth/providers/Auth0";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -7,6 +8,11 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
+    }),
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      issuer: process.env.AUTH0_ISSUER
     }),
   ]
 })
